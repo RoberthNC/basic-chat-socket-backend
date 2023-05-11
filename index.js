1bc4ab819e9a64bd7652148beeb3e83c49d31062
Roberto Núñez <robertonc1599@gmail.com>
2023-05-10 23:20:13 -0500

    85% of the project has been finished, but the socket.on of receive_message is not working

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+const { Server } = require("socket.io")
+
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
+
+const server = app.listen(3001, ()=>{
+    console.log("Server is running")
+})
+
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    }
+})
+
+io.on("connect", (socket)=>{
+    console.log(`User Connected: ${socket.id}`)
+
+    socket.on("join_room",(room)=>{
+        socket.to(room)
+        console.log(`User ${socket.id} has joined to the room ${room}`)
+    })
+
+    socket.on("send_message",(messageData)=>{
+        socket.to(messageData.room).emit("receive_message", messageData)
+    })
+
+    socket.on("disconnect", ()=>{
+        console.log(`User Disconnected: ${socket.id}`)
+    })
+})
